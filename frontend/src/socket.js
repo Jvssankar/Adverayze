1621fbeba4fca3c69ad8d58a1b02350c27efd723
Jakka Vidya Sai Sankar <154214159+Jvssankar@users.noreply.github.com>
2026-04-06 21:20:18 +0530

    update socket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,7 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-const socket = io("https://adverayze.onrender.com");
+const SOCKET_URL = "https://adverayze.onrender.com";
 
-export const socket = io(SOCKET_URL, { autoConnect: false });
+export const socket = io(SOCKET_URL, {
+  autoConnect: false,
+});
